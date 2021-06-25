29c19c52d9e45d89cb906bf473968adb1eb552f9
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-25 14:50:56 +0200

    minor fixes

--- instructor-code/01_code-express_get_query/app.js
+++ instructor-code/01_code-express_get_query/app.js
@@ -6,7 +6,7 @@
  const express = require("express");
  const hbs = require("hbs");
  const app = express();
- const PORT = 3000;
+ const PORT = 3001;
  
  // SET THE TEMPLATE ENGINE
  app.set("view engine", "hbs");
@@ -23,7 +23,7 @@
  // ROUTES
  app.get("/", (req, res) => {
    console.log(req);
-   res.send("You reached the root of the server");
+   res.render("index");
  });
 
- app.listen(3000)
+ app.listen(PORT)
