765d27fbd9704616c0a706fa36ed2c6ab1e53d59
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-25 15:18:59 +0200

    WIP step 1 query params

--- instructor-code/01_code-express_get_query/app.js
+++ instructor-code/01_code-express_get_query/app.js
@@ -6,24 +6,33 @@
  const express = require("express");
  const hbs = require("hbs");
  const app = express();
+ const path = require('path')
  const PORT = 3001;
  
+ // REGISTER THE PARTIAL
+ hbs.registerPartials(path.join(__dirname +  "/views/partials"));
+
  // SET THE TEMPLATE ENGINE
  app.set("view engine", "hbs");
- app.set("views", __dirname + "/views");
+ app.set("views", path.join(__dirname + "/views"));
  
  // SET THE STATIC FOLDER FOR PUBLIC FILES
- app.use(express.static(__dirname + "/public"));
+ app.use(express.static(path.join(__dirname + "/public")));
  
- // REGISTER THE PARTIAL
- hbs.registerPartials(__dirname + "/views/partials");
  
  // MIDDLEWARE
  
  // ROUTES
+ // localhost:3000?q=how%20are%20you&username=Marco
  app.get("/", (req, res) => {
-   console.log(req);
-   res.render("index");
+   console.log(req.query);
+   const q = req.query.q
+   const username = req.query.username
+   res.render("index", {q, username});
  });
+
+app.get("/", (req,res) => {
+
+})
 
  app.listen(PORT)
