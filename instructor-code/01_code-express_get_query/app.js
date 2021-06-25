c0e450ff68350266f0d25a3e8f5c08349ae04386
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-25 15:56:21 +0200

    fixed teh form partial

--- instructor-code/01_code-express_get_query/app.js
+++ instructor-code/01_code-express_get_query/app.js
@@ -9,9 +9,7 @@
  const path = require('path')
  const PORT = 3001;
  
- // REGISTER THE PARTIAL
- hbs.registerPartials(path.join(__dirname, "/views/partials"));
-
+ 
  // SET THE TEMPLATE ENGINE
  app.set("view engine", "hbs");
  app.set("views", path.join(__dirname, "/views"));
@@ -19,9 +17,9 @@
  // SET THE STATIC FOLDER FOR PUBLIC FILES
  app.use(express.static(path.join(__dirname, "/public")));
  
- 
- // MIDDLEWARE
- 
+ // REGISTER THE PARTIAL
+ hbs.registerPartials(path.join(__dirname, "views/partials"));
+
  // ROUTES
  // localhost:3000?q=how%20are%20you&username=Marco
  app.get("/", (req, res) => {
@@ -45,9 +43,9 @@
 // This ia a dynamic URL parameter that is available to me in req.params
 // If I receive a URL of the form localhost:3000/english
 // then english will be the value of my language parameter
-app.get("/:language", (req, res)=>{
+app.get("/news/:language", (req, res)=>{
   const lang = req.params.language
-  res.render("index", {lang});
+  res.render("news", {lang});
 })
 
 
