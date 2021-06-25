06aefbd402bef00dc8be7f48cdf5dfcd65f82b4f
Viciot <vfuentesnoguera@gmail.com>
2021-06-25 16:09:37 +0200

    work get

--- 01_code-express_get_query/app.js
+++ 01_code-express_get_query/app.js
@@ -6,24 +6,49 @@
  const express = require("express");
  const hbs = require("hbs");
  const app = express();
- const PORT = 3000;
- 
- // SET THE TEMPLATE ENGINE
- app.set("view engine", "hbs");
- app.set("views", __dirname + "/views");
- 
- // SET THE STATIC FOLDER FOR PUBLIC FILES
- app.use(express.static(__dirname + "/public"));
+ const path = require('path')
+ const PORT = 3001;
  
  // REGISTER THE PARTIAL
- hbs.registerPartials(__dirname + "/views/partials");
+ hbs.registerPartials(path.join(__dirname, "/views/partials"));
+
+ // SET THE TEMPLATE ENGINE
+ app.set("view engine", "hbs");
+ app.set("views", path.join(__dirname, "/views"));
+ 
+ // SET THE STATIC FOLDER FOR PUBLIC FILES
+ app.use(express.static(path.join(__dirname, "/public")));
+ 
  
  // MIDDLEWARE
  
  // ROUTES
+ // localhost:3000?q=how%20are%20you&username=Marco
  app.get("/", (req, res) => {
-   console.log(req);
-   res.send("You reached the root of the server");
- });
+   console.log(req.query);
+   const q = req.query.q
+   const username = req.query.username
+   res.render("index", {q, username});
+  });
+  
+  app.get("/search", (req,res) => {
+    
+    const city = req.query.city
+    
+    const startDate = req.query['start-date']
+    
+    const endDate = req.query['end-date']
+    res.render("search", {city, startDate, endDate});
 
- app.listen(3000)
+})
+
+// This ia a dynamic URL parameter that is available to me in req.params
+// If I receive a URL of the form localhost:3000/english
+// then english will be the value of my language parameter
+app.get("/news/:language", (req, res)=>{
+  const lang = req.params.language
+  res.render("index", {lang});
+})
+
+
+ app.listen(PORT)
